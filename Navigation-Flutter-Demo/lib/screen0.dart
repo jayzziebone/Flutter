bbe3ae2fdd897bb57187fd17aec63fcf2d8926a3
jayzziebone <junior.lutula@gmail.com>
2022-01-11 10:12:07 -0500

    Update Navigation folders

---
+++ Navigation-Flutter-Demo/lib/screen0.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'screen2.dart';
+import 'screen1.dart';
+
+class Screen0 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text('Screen 0'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red)
+              ),
+              child: Text('Go To Screen 1'),
+              onPressed: () {
+                //Navigate to Screen 1
+                Navigator.pushNamed(context, '/first');
+              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)
+              ),
+              child: Text('Go To Screen 2'),
+              onPressed: () {
+                //Navigate to Screen 2
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
